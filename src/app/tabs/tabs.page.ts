a29387c10680858cf13fee2a947ba198e248bd61
Antonio Freire Costa Filho <antonio.costa@ac01.colortel.com.br>
2020-01-31 14:02:44 -0300

    Primeiro commit

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ScreensizeService } from '../services/screensize.service';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  isDesktop: boolean;
 
+  constructor(private screensizeService: ScreensizeService) {
+
+    this.screensizeService.isDesktopView().subscribe(isDesktop => {
+      if (this.isDesktop && !isDesktop) {
+        // Reload because our routing is out of place
+        window.location.reload();
+      }
+      this.isDesktop = isDesktop;
+    })
+  }
 }
